327e3ea4260f2854aea28cff4409396ddffdf188
Spiros Boosalis <samboosalis@gmail.com>
2016-05-30 17:47:35 +0300

    2nd

--- sources/Vinyl/Effects.hs
+++ sources/Vinyl/Effects.hs
@@ -1,2 +1,17 @@
+{-# LANGUAGE ConstraintKinds, FlexibleContexts, PatternSynonyms #-}
+{-# LANGUAGE TemplateHaskell #-}
+-- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+-- {-# OPTIONS_GHC -ddump-splices #-}
+-- for `makeFree`
+
+{-|
+
+-}
 module Vinyl.Effects where
 import Vinyl.Effects.Extra()
+
+import Data.Vinyl
+import Control.Monad.Trans.Free
+import Control.Monad.Free.TH       (makeFree)
+-- import Control.Comonad.Trans.Cofree
--- sources/Vinyl/Effects/Extra.hs
+++ sources/Vinyl/Effects/Extra.hs
@@ -1,9 +1,12 @@
 module Vinyl.Effects.Extra
- ( module Data.Vinyl
+ ( module Vinyl.Effects.Extra
+ -- , module Data.Vinyl
  , module GHC.Generics
  , module Data.Data
  ) where
 
-import Data.Vinyl
 import GHC.Generics (Generic)
 import Data.Data (Data)
+
+nothing :: Monad m => m ()
+nothing = return ()
