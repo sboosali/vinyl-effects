8f4ce5700d1160e51e17799bc3269a4b611ea56f
Spiros Boosalis <samboosalis@gmail.com>
2016-05-31 02:05:31 +0300

    docs

--- sources/Vinyl/Effects.hs
+++ sources/Vinyl/Effects.hs
@@ -1,6 +1,8 @@
-{-| extensible effects. good inference.
+{-| Extensible effects. Minimal boilerplate. Decent inference.
 
-uses:
+See "Vinyl.Effects.Example" for a detailed tutorial.
+
+Dependencies:
 
 * the @free@ package,
 for 'Free' (which transforms a functor into a monad)
@@ -9,20 +11,47 @@ and 'Cofree' (which transforms a functor into a comonad).
 for (TODO) 'CoRec' (an n-ary, un-nested sum type)
 and 'Rec' (an n-ary, un-nested product type).
 
-background (necessary only for contributing to the library, not using it):
+Concepts (necessary only for contributing to the library, not using it):
 
 * for composing domain-specific languages and interpeters (parts 1 to 5):
-    * <http://dlaing.org/cofun/posts/free_and_cofree.html>
-    * <http://dlaing.org/cofun/posts/monad_transformers_and_comonad_transformers.html>
-    * <http://dlaing.org/cofun/posts/coproducts_for_free_and_products_for_cofree.html>
-    * <http://dlaing.org/cofun/posts/pairing_and_io.html>
-    * <http://dlaing.org/cofun/posts/pairing_over_the_network.html>
-    * (this series a tour-de-force, read it!).
 
-* for "free monads": <> TODO tekmos blog
-* : <> TODO
-* for "extensible effects": <> TODO ocharles
-* for @mtl@: <> TODO
+     * <http://dlaing.org/cofun/posts/free_and_cofree.html>
+     * <http://dlaing.org/cofun/posts/monad_transformers_and_comonad_transformers.html>
+     * <http://dlaing.org/cofun/posts/coproducts_for_free_and_products_for_cofree.html>
+     * <http://dlaing.org/cofun/posts/pairing_and_io.html>
+     * <http://dlaing.org/cofun/posts/pairing_over_the_network.html>
+     * (this series a tour-de-force, read it!).
+
+* for "extensible records":
+
+     * <https://hackage.haskell.org/package/vinyl-0.5.2/docs/Data-Vinyl-Tutorial-Overview.html>
+
+* for "free monads":
+
+     * <> TODO tekmos blog
+
+* for "extensible effects":
+
+     * <> TODO ocharles
+
+* for @mtl@:
+
+     * <> TODO
+
+Reverse Dependencies:
+
+* packdeps.haskellers.com/reverse/vinyl-effects
+* TODO <workflow-types> uses this package to maximize the effects the
+platform-independent monad provides, while still preserving the richer effects
+the platform-specific monads provide (in <workflow-linux>, <workflow-osx>, and
+<workflow-windows>). e.g. @OSX@ can easily access the clipboard via @bash@.
+
+Existing Alternatives:
+
+* <extensible effects>: uses @-XOverloadedInstances@. bad inference?
+* <> romansch:
+* <> ocharles:
+* <> mtl: doesn't compose
 
 -}
 module Vinyl.Effects
--- sources/Vinyl/Effects/Example.hs
+++ sources/Vinyl/Effects/Example.hs
@@ -1,10 +1,87 @@
 {-# LANGUAGE ConstraintKinds, NoMonomorphismRestriction, DataKinds #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
-{-| (see source).
+{-| This module defines two effects ('Clipboard' and 'OpenUrl'),
+and then composes them ('Workflow') "on-the-fly".
+
+For each effect, we:
+
+* Define a functor (e.g. 'ClipboardF').
+(optionally (for convenience), aliases for the constraint
+(e.g. 'MonadClipboard') and effect-set (e.g. 'Clipboard')).
+* Define overloaded constructors (e.g. 'getClipboard', 'setClipboard'). TODO th.
+* Define a handler (e.g. @('handleClipboard' :: 'ClipboardF' ('IO' a) -> 'IO' a)@),
+which involves minimal boilerplate. (if you've used the @free@ package,
+you know how it's done).
+Then, wrap that handler (a 'CoAlgebra') in an 'Interpreter',
+for /extensibility/.
+
+You can use these effects extensibly, with an "@mtl@-style". e.g.
+Since 'getClipboard' and 'openUrl' are overloaded, they can both be used in
+'openFromClipboard'.
+
+@
+'openFromClipboard' = do   -- :: ('MonadClipboard' m, 'MonadOpenUrl' m) => m ()
+  s \<- 'getClipboard'      -- :: ('MonadClipboard' m                ) => m String
+  'openUrl' s              -- :: (                  'MonadOpenUrl' m) => m ()
+@
+
+Note:
+
+* the type of @openFromClipboard@ is inferred.
+* the constraints are aliases; you don't need to write a new class
+for each new effect type.
+* for compositions of effects (like 'MonadWorkflow'),
+you don't even need to write a new type.
+Just append the interpreters you want (with `appendInterpreters`).
 
 -}
-module Vinyl.Effects.Example where
+module Vinyl.Effects.Example
+ ( main
+
+ -- * Effect #1: Clipboard
+ ,MonadClipboard
+ ,Clipboard
+ ,ClipboardF(..)
+ -- ** overloaded constructors
+ ,getClipboard
+ ,setClipboard
+ -- ** e.g. reverseClipboard
+ ,reverseClipboard
+ -- ** the interpreter
+ ,runClipboard
+ ,interpreterClipboard
+ ,handleClipboard
+ -- ** the implementation
+ ,sh_GetClipboard
+ ,sh_SetClipboard
+
+ -- * Effect #2: Clipboard
+ ,MonadOpenUrl
+ ,OpenUrl
+ ,OpenUrlF
+ -- ** overloaded constructors
+ ,openUrl
+ -- ** the interpreter
+ ,runOpenUrl
+ ,interpreterOpenUrl
+ -- ** the implementation
+ ,sh_OpenUrl
+
+ -- * Workflow: \#1 + \#2
+ , MonadWorkflow
+ , Workflow
+ , runWorkflow
+ , interpretWorkflow
+
+ -- ** e.g. openFromClipboard
+ , openFromClipboard
+ , openFromClipboard_nothingSpecialized
+ , openFromClipboard_monadSpecialized
+ , openFromClipboard_effectsSpecialized
+ , openFromClipboard_bothSpecialized
+
+ ) where
 import Vinyl.Effects
 
 import Control.Arrow ((>>>))
@@ -13,7 +90,44 @@ import GHC.IO.Handle (hGetContents)
 
 --------------------------------------------------------------------------------
 
+{-| run with:
+
+@
+stack build && stack exec example-vinyl-effects
+@
+
+(read the source too).
+
+-}
+main :: IO ()
+main = do
+ putStrLn ""
+
+ -- runOpenUrl $ openUrl "http://google.com"
+
+ -- runClipboard $ setClipboard "'" -- { echo '''' | pbcopy } would fail, unless propertly escaped
+
+ -- runClipboard $ setClipboard "http://google.com"
+ -- runWorkflow $ openFromClipboard
+ --
+ -- runClipboard $ reverseClipboard
+ -- print =<< runClipboard getClipboard
+
+ runClipboard $ do
+   setClipboard "http://google.com"   -- `setClipboard` as a `Clipboard`
+
+ s <- runWorkflow $ do
+   openFromClipboard
+   reverseClipboard                   -- `setClipboard` as a `Workflow`
+   getClipboard
+
+ print s
+
+--------------------------------------------------------------------------------
+
 {- | an effect to visit the url that's currently in the clipboard.
+
+uses two distinct effects, i.e. it's a 'Workflow' action.
 
 @
 openFromClipboard = do
@@ -35,7 +149,7 @@ Inferred (with @NoMonomorphismRestriction@):
 @
  :: ( 'MonadLanguage' m effects
     , 'RElem' ClipboardF effects ('RIndex' ClipboardF effects)
-    , 'RElem' OpenUrlF effects ('RIndex' OpenUrlF effects)
+    , 'RElem' OpenUrlF   effects ('RIndex' OpenUrlF   effects)
     )
  => m ()
 @
@@ -101,14 +215,24 @@ openFromClipboard_bothSpecialized = openFromClipboard
 
 --------------------------------------------------------------------------------
 
+-- | a constraint (with @-XConstraintKinds@).
 type MonadWorkflow m effects =
    ( MonadClipboard m effects
    , MonadOpenUrl   m effects
    )
 
+-- | a set of two effects.
 type Workflow = '[ClipboardF,OpenUrlF]
 
 {-| run an ad-hoc grouping of two effects.
+
+@
+runWorkflow = 'interpretLanguage' interpretWorkflow
+@
+
+can run any action of type:
+
+@('MonadWorkflow' m effects) => m a@
 
 -}
 runWorkflow :: Language Workflow a -> IO a
@@ -120,7 +244,8 @@ runWorkflow = interpretLanguage interpretWorkflow
 interpretWorkflow = 'appendInterpreters' 'interpreterClipboard' 'interpreterOpenUrl'
 @
 
-no new @Either@-like @data@types needed, the @type@-aliases are only for clarity.
+no new @Either@-like @data@types needed,
+the @type@-aliases are only for clarity.
 
 -}
 interpretWorkflow :: Interpreter IO Workflow a
@@ -128,13 +253,16 @@ interpretWorkflow = interpreterClipboard `appendInterpreters` interpreterOpenUrl
 
 --------------------------------------------------------------------------------
 
+-- | the constraint
 type MonadClipboard m effects =
   ( MonadLanguage m effects
   , ClipboardF ∈ effects
   )
 
+-- | the set of effects (one)
 type Clipboard = '[ClipboardF]
 
+-- | the functor
 data ClipboardF k
  = GetClipboard (String -> k)
  | SetClipboard String k
@@ -150,7 +278,7 @@ setClipboard :: (MonadClipboard m effects) => String -> m ()
 setClipboard s = liftL $ SetClipboard s ()
   -- SetClipboard s () :: ClipboardF ()
 
--- | (derived from the two primitves).
+-- | derived from the two primitves.
 reverseClipboard :: (MonadClipboard m effects) => m ()
 reverseClipboard = getClipboard >>= (reverse >>> setClipboard)
 
@@ -166,11 +294,17 @@ runClipboard = 'iterTM' handleClipboard
 runClipboard :: Language '[ClipboardF] a -> IO a
 runClipboard = interpretLanguage interpreterClipboard
 
--- | wraps 'handleClipboard'
+-- | @interpreterClipboard = 'singletonInterpreter' 'handleClipboard'@
 interpreterClipboard :: Interpreter IO '[ClipboardF] a
 interpreterClipboard = singletonInterpreter handleClipboard
 
 {- | glue the functor to its effects.
+
+@
+handleClipboard = \case
+  'GetClipboard' f   -> 'sh_GetClipboard' >>= f
+  'SetClipboard' s k -> 'sh_SetClipboard' s >> k
+@
 
 -}
 handleClipboard :: CoAlgebra ClipboardF (IO a)
@@ -199,13 +333,16 @@ sh_SetClipboard s = do
 
 --------------------------------------------------------------------------------
 
+-- | the constraint
 type MonadOpenUrl m effects =
   ( MonadLanguage m effects
   , OpenUrlF ∈ effects
   )
 
+-- | the set of effects (one)
 type OpenUrl = '[OpenUrlF]
 
+-- | the functor
 data OpenUrlF k -- TODO name OpenFile, works for any file
  = OpenUrl String k
  deriving Functor
@@ -215,11 +352,30 @@ openUrl :: (MonadOpenUrl m effects) => String -> m ()
 openUrl s = liftL $ OpenUrl s ()
    -- OpenUrl s :: OpenUrlF ()
 
--- | calls 'interpretLanguage'.
+{- |
+
+@
+runOpenUrl = interpretLanguage interpreterOpenUrl
+@
+
+-}
 runOpenUrl :: Language '[OpenUrlF] a -> IO a
 runOpenUrl = interpretLanguage interpreterOpenUrl
 
--- |
+{- |
+
+@
+interpreterOpenUrl = 'singletonInterpreter' $ \case
+  'OpenUrl' s k -> 'sh_OpenUrl' s >> k
+@
+
+can extract the "co-algebra" with
+
+@
+handleOpenUrl = 'fromSingletonInterpreter' interpreterOpenUrl
+@
+
+-}
 interpreterOpenUrl :: Interpreter IO '[OpenUrlF] a
 interpreterOpenUrl = singletonInterpreter $ \case
   OpenUrl s k -> sh_OpenUrl s >> k
@@ -231,38 +387,5 @@ sh_OpenUrl s = do
       (proc "open" [s])
   _ <- waitForProcess _process
   return ()
-
---------------------------------------------------------------------------------
-
-{-|
-
-@
-stack build && stack exec example-vinyl-effects
-@
-
--}
-main :: IO ()
-main = do
- putStrLn ""
-
- -- runOpenUrl $ openUrl "http://google.com"
-
- -- runClipboard $ setClipboard "'" -- { echo '''' | pbcopy } would fail, unless propertly escaped
-
- -- runClipboard $ setClipboard "http://google.com"
- -- runWorkflow $ openFromClipboard
- --
- -- runClipboard $ reverseClipboard
- -- print =<< runClipboard getClipboard
-
- runClipboard $ do
-   setClipboard "http://google.com"   -- `setClipboard` as a `Clipboard`
-
- s <- runWorkflow $ do
-   openFromClipboard
-   reverseClipboard                   -- `setClipboard` as a `Workflow`
-   getClipboard
-
- print s
 
 --------------------------------------------------------------------------------
